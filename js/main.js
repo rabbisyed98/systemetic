3fccd7374e94cff4fa46b315f3bec7bc48eeb055
Rabbi Syed <rabbisyed98@gmail.com>
2021-04-10 20:37:07 +0600

    All files are added  via upload.
    All files are uploaded!

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// BX Slider JS
+$(document).ready(function () {
+  $('.slider_area').bxSlider();
+  $('#main_menu').meanmenu({
+    meanMenuContainer: '#mobile_menu',
+    meanScreenWidth: 991,
+  });
+});
